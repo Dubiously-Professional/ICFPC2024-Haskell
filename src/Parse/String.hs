f3d12cbafe1f89a1316f0131a870e62e46368b0e
Joel Croteau <joelcroteau@joelcroteau.com>
2025-06-08 02:54:20 +0000

    Model A's improvement to translateByte

--- src/Parse/String.hs
+++ src/Parse/String.hs
@@ -4,19 +4,24 @@ module Parse.String (decodeString) where
 import qualified Data.ByteString.Lazy as LBS
 import qualified Data.Text as T
 import Data.Word (Word8)
+import Data.Array (Array, array, (!), bounds, inRange)
 
--- Translation table for codes 33-126 (94 characters)
+-- Translation table for codes 33-126 (94 characters)                                                                                                 
 translationTable :: String
 translationTable = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`|~ \n"
 
+-- Pre-computed array for O(1) lookups
+translationArray :: Array Int Char
+translationArray = array (33, 126) (zip [33..126] translationTable)
+
 decodeString :: LBS.ByteString -> Maybe T.Text
-decodeString lbs = 
+decodeString lbs =
     let bytes = LBS.unpack lbs
     in fmap T.pack (mapM translateByte bytes)
 
 translateByte :: Word8 -> Maybe Char
 translateByte byte
-    | byte >= 33 && byte <= 126 = 
-        let index = fromIntegral byte - 33
-        in Just (translationTable !! index)
+    | inRange (bounds translationArray) index = Just (translationArray ! index)
     | otherwise = Nothing
+  where
+    index = fromIntegral byte
